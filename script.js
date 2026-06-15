230b7cb56c10f2d5a6cd6cb688c7c655123a969c
Tyrion122 <167515538+Tyrion122@users.noreply.github.com>
2026-06-15 20:54:18 +0200

    Refactor button logic and add questions/answers

--- script.js
+++ script.js
@@ -1,48 +1,76 @@
-let buttons = document.querySelectorAll(".buttonAnswer");
-let index=0;
-console.log(buttons);
-//...............
-let right = document.querySelectorAll(".true");//це ідея з прослуховуванням лише правильних/робота  з правильною відповіддю
-//...............
+let answers = [
+    "Answer1",
+    "Answer2",
+    "Answer3",
+    "Answer4",
+    "Answer5",
+    "Answer6",
+    "Answer7",
+    "Answer8",
+    "Answer9",
+    "Answer10",
+    "Answer11",
+    "Answer12",
+    "Answer13",
+    "Answer14",
+    "Answer15"
+]
 
-//всі кнопки заміняються одним індексом а не різним
-//добавити зміну правильної відповіді (може всі класи видалити і заново створити?)
+let questions = [
+    "Question1",
+    "Question2",
+    "Question3",
+    "Question4",
+    "Question5"
+]
+let question = document.getElementById("question");
 
-buttons.forEach((but)=>{
-    but.addEventListener("click", ()=>{
-        console.log(but);//but це обєкт (там ціла кнопка виводиться)
-        //console.log(buttons.item);
-        if (but.classList.contains(true)){
+const buttonArr = [
+    document.getElementById("but1"),
+    document.getElementById("but2"),
+    document.getElementById("but3")
+];
+
+let a=3;
+let q=1;
+function ButtonChange(){
+    for(let b=0; b<buttonArr.length; b++){
+        buttonArr[b].textContent=answers[a];
+        buttonArr[b].classList.remove("false", "true");
+        a++;
+    }
+    let ran = Math.floor(Math.random() * 3);
+    buttonArr[ran].classList.add("true");
+    question.textContent=questions[q];
+    q++;
+};
+
+let score = 0;
+let finalScore = 0;
+let num = 0;
+for(let butt in buttonArr){
+    buttonArr[butt].addEventListener("click",(event) =>{
+        if(event.target.classList.contains("true")){
             alert("true");
-            ButtonChange(but);
+            score++;
+            ButtonChange();
         }else{
             alert("Try again");
+            ButtonChange();
+        }
+        num++;
+        if(num==questions.length){
+            finalScore = (score/num) * 100 ;
+            alert("Your score:" + Math.round(finalScore) +"%" + "\n"+ score + "/" + num);
+            location.reload();
         }
     });
-    index++;
-});
-
-function ButtonChange(){
-    buttons.forEach((but)=>{
-        but.textContent= "Answer"+index;
-})};
-
-//можливий варік але треба думати
-
-/*
-buttons.forEach((but)=>{
-    index++;
-    but.addEventListener("click", ButtonChange(index))});
-
-function ButtonChange(index){
-    if target {
-            alert("true");
-            buttons.forEach((but)=>{
-                but.textContent= "Answer"+index;
-            });
-    }else{
-            alert("Try again");
-    }
     
 }
-*/
+window.onload=()=>{
+    for(let b=0; b<buttonArr.length; b++){
+        buttonArr[b].classList.remove("false", "true");
+    }
+    let ran = Math.floor(Math.random() * 3);
+    buttonArr[ran].classList.add("true");
+}
